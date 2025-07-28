006b520580e8370edea16847d9980d2cbf2ed05d
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-28 15:57:17 -0300

    Repository agregado para Topicos

---
+++ src/main/java/com/alura/forohub/repository/TopicoRepository.java
@@ -0,0 +1,8 @@
+package com.alura.forohub.repository;
+
+import com.alura.forohub.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+}
