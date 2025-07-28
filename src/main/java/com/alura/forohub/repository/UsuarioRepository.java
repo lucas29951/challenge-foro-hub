08fe7eb3334d794c7450a9d498b750b297f9585d
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-28 17:44:03 -0300

    Metodo agregado al repository de Usuario

--- src/main/java/com/alura/forohub/repository/UsuarioRepository.java
+++ src/main/java/com/alura/forohub/repository/UsuarioRepository.java
@@ -2,7 +2,9 @@ package com.alura.forohub.repository;
 
 import com.alura.forohub.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    UserDetails findByCorreoElectronico(String correoElectronico);
 }
