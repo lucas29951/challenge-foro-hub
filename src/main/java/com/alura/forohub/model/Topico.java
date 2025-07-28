8e0ec29c976ce5cf310fe9f81e85fa17fe6883ba
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-28 17:11:27 -0300

    Actualizacion del modelo de Topico

--- src/main/java/com/alura/forohub/model/Topico.java
+++ src/main/java/com/alura/forohub/model/Topico.java
@@ -1,6 +1,9 @@
 package com.alura.forohub.model;
 
+import com.alura.forohub.dto.topicos.DatosActualizacionTopico;
+import com.alura.forohub.dto.topicos.DatosRegistroTopico;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -29,4 +32,35 @@ public class Topico {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "curso")
     private Curso curso;
+
+    public Topico(DatosRegistroTopico datos) {
+        this.id = null;
+        this.titulo = datos.titulo();
+        this.mensaje = datos.mensaje();
+        this.fechaCreacion = datos.fechaCreacion();
+        this.status = datos.status();
+        this.autor = datos.autor();
+        this.curso = datos.curso();
+    }
+
+    public void actualizarInformacion(@Valid DatosActualizacionTopico datos) {
+        if (datos.titulo() != null) {
+            this.titulo = datos.titulo();
+        }
+        if (datos.mensaje() != null) {
+            this.mensaje = datos.mensaje();
+        }
+        if (datos.fechaCreacion() != null) {
+            this.fechaCreacion = datos.fechaCreacion();
+        }
+        if (datos.status() != null) {
+            this.status = datos.status();
+        }
+        if (datos.autor() != null) {
+            this.autor = datos.autor();
+        }
+        if (datos.curso() != null) {
+            this.curso = datos.curso();
+        }
+    }
 }
