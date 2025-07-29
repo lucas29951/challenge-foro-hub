5daf3a1982bdbb1b302019a3f72465e9b39a6126
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-28 21:47:00 -0300

    Se agrego un paquete con Validaciones y Gestion de Errores

---
+++ src/main/java/com/alura/forohub/exceptions/GestorDeErrores.java
@@ -0,0 +1,35 @@
+package com.alura.forohub.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GestorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity gestionarError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity gestionarError400(MethodArgumentNotValidException ex) {
+        var errores = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
+    }
+
+    @ExceptionHandler(ValidacionException.class)
+    public ResponseEntity gestionarErrorDeValidacion(ValidacionException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    public record DatosErrorValidacion(String campo, String mensaje) {
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
---
+++ src/main/java/com/alura/forohub/exceptions/ValidacionException.java
@@ -0,0 +1,8 @@
+package com.alura.forohub.exceptions;
+
+public class ValidacionException extends RuntimeException {
+
+    public ValidacionException(String mensaje) {
+        super(mensaje);
+    }
+}
