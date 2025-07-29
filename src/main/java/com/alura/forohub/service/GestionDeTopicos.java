64a48ce929ce0d84cd56aa68047c98e24eb4408b
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-29 18:14:38 -0300

    Update: metodo actualizar agregado el service

--- src/main/java/com/alura/forohub/service/GestionDeTopicos.java
+++ src/main/java/com/alura/forohub/service/GestionDeTopicos.java
@@ -1,5 +1,6 @@
 package com.alura.forohub.service;
 
+import com.alura.forohub.dto.topicos.DatosActualizacionTopico;
 import com.alura.forohub.dto.topicos.DatosDetalleTopico;
 import com.alura.forohub.dto.topicos.DatosRegistroTopico;
 import com.alura.forohub.exceptions.ValidacionException;
@@ -23,16 +24,39 @@ public class GestionDeTopicos {
     private TopicoRepository topicoRepository;
 
     public DatosDetalleTopico postear(DatosRegistroTopico datos) {
-        if (!usuarioRepository.existsById(datos.autor().getId())) {
+        var usuario = usuarioRepository.getReferenceById(datos.autor());
+        var curso = cursoRepository.getReferenceById(datos.curso());
+
+        if (usuario.getId() == null) {
             throw new ValidacionException("No existe un usuario con id indicado.");
         }
-        if (!cursoRepository.existsById(datos.curso().getId())) {
+        if (curso.getId() == null) {
             throw new ValidacionException("No existe un curso con el id indicado.");
         }
 
-        var topico = new Topico(datos);
+        var topico = new Topico(datos, usuario, curso);
 
         topicoRepository.save(topico);
+
+        return new DatosDetalleTopico(topico);
+    }
+
+    public DatosDetalleTopico actualizar(DatosActualizacionTopico datos) {
+        var topico = topicoRepository.getReferenceById(datos.id());
+        var usuario = usuarioRepository.getReferenceById(datos.autor());
+        var curso = cursoRepository.getReferenceById(datos.curso());
+
+        if (topico.getId() == null) {
+            throw new ValidacionException("No existe un topico con el id indicado.");
+        }
+        if (usuario.getId() == null) {
+            throw new ValidacionException("No existe un usuario con id indicado.");
+        }
+        if (curso.getId() == null) {
+            throw new ValidacionException("No existe un curso con el id indicado.");
+        }
+
+        topico.actualizarInformacion(datos, usuario, curso);
 
         return new DatosDetalleTopico(topico);
     }
