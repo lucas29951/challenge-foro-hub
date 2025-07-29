b05936fd2a7b52fefdd317e260d612259d7055be
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-29 02:02:13 -0300

    Actualizacion del metodo find del repositorio

--- src/main/java/com/alura/forohub/repository/UsuarioRepository.java
+++ src/main/java/com/alura/forohub/repository/UsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    UserDetails findByCorreoElectronico(String correoElectronico);
+    UserDetails findByEmail(String email);
 }
