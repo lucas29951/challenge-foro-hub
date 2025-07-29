79fa4b946e33a8f3af9d56d9e355e799297bb82e
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-29 02:05:52 -0300

    Actualizacion del metodo generarToken

--- src/main/java/com/alura/forohub/service/TokenService.java
+++ src/main/java/com/alura/forohub/service/TokenService.java
@@ -24,7 +24,7 @@ public class TokenService {
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Foro Hub")
-                    .withSubject(usuario.getNombre())
+                    .withSubject(usuario.getEmail())
                     .withExpiresAt(fechaExpiracion())
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
