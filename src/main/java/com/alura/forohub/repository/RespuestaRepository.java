e90c8646ce5904c9942b1b093ef80d0a1cb0cce3
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-07-29 21:14:52 -0300

    Se agrego el repository correspondiente al modelo de Respuesta

---
+++ src/main/java/com/alura/forohub/repository/RespuestaRepository.java
@@ -0,0 +1,8 @@
+package com.alura.forohub.repository;
+
+import com.alura.forohub.model.Respuesta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
+
+}
